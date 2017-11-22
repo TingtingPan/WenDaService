a80bdbecdf046a706d32245fa211cd7a9774ca03
PanTingting <124777966@qq.com>
2017-11-22 21:19:16 +0800

    1、add register Service (not complete yet)

--- src/main/java/com/example/wenDaService/service/UserService.java
+++ src/main/java/com/example/wenDaService/service/UserService.java
@@ -2,8 +2,13 @@ package com.example.wenDaService.service;
 
 import com.example.wenDaService.dao.UserDAO;
 import com.example.wenDaService.model.User;
+import freemarker.template.utility.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by pantingting on 2017/8/13.
@@ -16,6 +21,19 @@ public class UserService {
 
     public User getUser(int id){
         return userDAO.selectByid(id);
+    }
+
+    public Map<String,String> register(String userName,String password){
+        Map<String,String> map = new HashMap<>();
+        if (StringUtils.isEmpty(userName)){
+            map.put("msg","用户名不能为空");
+        }
+        if (StringUtils.isEmpty(password)){
+            map.put("msg","密码不能为空");
+                    
+        }
+        /// TODO: 2017/11/22 0022  register and login 
+        return map;
     }
 }
 
